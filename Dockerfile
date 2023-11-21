87d1a4426bb799e23006a51f779ee7a32e6bf2b9
Lukas DAR <60785752+Lukas-De-Angelis-Riva@users.noreply.github.com>
2023-11-20 21:42:26 -0300

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,28 @@
-FROM python:3.9.7-slim
+FROM python:3.9.14 
+
 COPY . /
 
+# Build Chrome driver 
 RUN apt-get update && \
-    apt-get upgrade -y && \
-    apt-get install -y git && \
-    apt-get install -y openjdk-11-jre-headless && \
-    apt-get clean;
+    apt-get install -y gnupg wget curl unzip --no-install-recommends && \
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && \
+    echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list && \
+    apt-get update -y && \
+    wget https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_114.0.5735.90-1_amd64.deb && \
+    apt-get install -y ./google-chrome-stable_114.0.5735.90-1_amd64.deb && \
+    CHROMEVER=$(google-chrome --product-version | grep -o "[^\.]*\.[^\.]*\.[^\.]*") && \
+    echo $CHROMEVER && \
+    DRIVERVER=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROMEVER") && \
+    echo $DRIVERVER && \
+    wget --continue -P /chromedriver "http://chromedriver.storage.googleapis.com/$DRIVERVER/chromedriver_linux64.zip"
+RUN unzip /chromedriver/chromedriver* -d /chromedriver 
 
-RUN java -jar scripts/browsermob-proxy-2.1.4/lib/browsermob-dist-2.1.4.jar --port 9090 &
+RUN cp chromedriver/chromedriver usr/bin/
+
+# Install Java runtime 
+RUN apt-get install -y default-jre
 
 RUN pip3 install git+https://github.com/cisagov/findcdn.git
 RUN pip install -r requirements.txt
 
-ENTRYPOINT ["/bin/sh"]
+ENTRYPOINT ["/bin/bash"]
--- scripts/collectResourcestoDepthN.py
+++ scripts/collectResourcestoDepthN.py
@@ -18,6 +18,7 @@ import selenium.webdriver.remote.utils as utils
 import subprocess
 import re
 from pathlib import Path
+import pathlib
 import statistics
 import findcdn
 import psutil
@@ -27,13 +28,159 @@ import csv
 from collections import defaultdict
 import socket
 import ssl
+from webdriver_manager.chrome import ChromeDriverManager
+from browsermobproxy.exceptions import ProxyServerError
+from selenium.common.exceptions import TimeoutException, WebDriverException
+
+project_path = os.path.dirname(os.path.abspath(__file__))
+
+print (project_path)
+
+def create_dir_if_not_exists(path):
+	pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+
+class HARGenerator():
+    # This class is used to interact with BrowserMob Proxy, see here: https://github.com/lightbody/browsermob-proxy
+    # Source code for Python API is: https://browsermob-proxy-py.readthedocs.io/en/latest/_modules/browsermobproxy/server.html#Server
+    # Please make sure your system has the required Java runtime for the server to run properly.
+
+	def __init__(self):
+		self.port = 8090
+		self.bmp_log_path = os.path.join(project_path, '..', 'logs', 'bmp')
+		create_dir_if_not_exists(self.bmp_log_path)
+		self.chrome_driver_log_path = os.path.join(project_path, '..', 'logs', 'chromedriver')
+		create_dir_if_not_exists(self.chrome_driver_log_path)
+		self.terminated = False
+
+		try:
+			# Pen.write(f"Initiating server...", color='OKGREEN', newline=False)
+			self.server = Server(
+			    # path=os.path.join(project_path, '..', 'browsermob-proxy-2.1.4', 'bin', 'browsermob-proxy'),
+			    path=os.path.join(project_path,'browsermob-proxy-2.1.4', 'bin', 'browsermob-proxy'),
+			    options={'port': self.port})
+            # Pen.write(f"OK", color='OKGREEN')
+
+            # Pen.write(f"Starting server...", color='OKGREEN', newline=False)
+			self.server.start(options={
+			    'log_file': f'{datetime.now().strftime("%Y-%m-%d-%H:%M:%S")}.log',
+			    'log_path': self.bmp_log_path,
+			    'retry_count': 5
+			})
+            # Pen.write(f"OK", color='OKGREEN')
+
+		except ProxyServerError as err:
+            # Pen.write(f"Error starting server. Please check server logs. Exiting...", color='FAIL')
+            # Pen.write(str(err), color='FAIL')
+			print (str(err))
+			exit(-1)
+
+        # Pen.write(f"Creating proxy server...", color='OKGREEN', newline=False)
+		self.proxy = self.server.create_proxy(params={"trustAllServers": "true"})
+        # Pen.write(f"OK", color='OKGREEN')
+
+        # Pen.write(f"Creating Chrome driver...", color='OKGREEN', newline=False)
+		self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=self._chrome_options())
+        # Pen.write(f"OK", color='OKGREEN')
+
+	def _chrome_options(self):
+		options = webdriver.ChromeOptions()
+		options.add_argument("--proxy-server={}".format(self.proxy.proxy))
+		options.add_argument("--ignore-ssl-errors=yes")
+		options.add_argument("--ignore-certificate-errors")
+		options.add_argument("--headless")
+		options.add_argument("--no-cache")
+		options.add_argument("--no-sandbox")
+		options.add_argument("--disable-dev-shm-usage")
+		options.add_argument("--verbose")
+		options.add_argument(f"--log-path={self.chrome_driver_log_path}")
+
+		return options
+
+	def __del__(self):
+		if not self.terminated:
+            # Pen.write(
+            #     f"Clean up on HARGenerator object deletion. Did you forget to explicitly terminate HARGenerator object?",
+            #     color='WARNING')
+			self.terminate()
+
+	def terminate(self):
+		try:
+            # BrowserMobProxy starts a process (parent process), which starts another process running the proxy server (child process)
+            # Calling self.server.stop() only stops the parent process but not the child process, which becomes a zombie process when the program ends
+            # Pen.write(f"Cleaning up HAR object...", color='OKGREEN')
+
+			for child in psutil.Process(self.server.process.pid).children(recursive=True):
+                # Pen.write(f"Killing child process {child.pid}...", color='OKGREEN', newline=False)
+				child.kill()
+                # Pen.write(f"OK", color='OKGREEN')
+            # Pen.write(f"Killing BMP server...", color='OKGREEN', newline=False)
+			self.server.stop()
+            # Pen.write(f"OK", color='OKGREEN')
+
+            # Pen.write(f"Killing Chrome driver...", color='OKGREEN', newline=False)
+			self.driver.quit()
+			time.sleep(1)  # This is to prevent "ImportError: sys.meta_path is None, Python is likely shutting down" from Selenium, see here: https://stackoverflow.com/questions/41480148/importerror-sys-meta-path-is-none-python-is-likely-shutting-down
+			# Pen.write(f"OK", color='OKGREEN')
+
+			self.terminated = True
+		# except ImportError:
+		# 	pass  # Ignore "ImportError: sys.meta_path is None, Python is likely shutting down"
+		except Exception as e:
+			print (str(e))
+
+    # def get_har(self, hostname):
+    #     self.proxy.new_har(hostname)
+    #     try:
+    #         self.driver.set_page_load_timeout(60)
+    #         self.driver.get(f'https://{hostname}')
+    #     except TimeoutException as err:
+    #         Pen.write(f'Timeout from renderer for {hostname}. Skipping...', 'FAIL')
+    #     except WebDriverException as err:
+    #         Pen.write(f'ERR_TUNNEL_CONNECTION_FAILED from renderer for {hostname}. Skipping...', 'FAIL')
+    #     except Exception as err:
+    #         Pen.write(f'Unexpected error: {err}.\n Skipping...', 'FAIL')
+
+    #     time.sleep(1)
+
+    #     return self.proxy.har
+
+    # def get_hars(self, hostnames):
+    #     hars = list()
+    #     for i, hostname in enumerate(hostnames):
+    #         hars.append(self.get_har(hostname))
+
+    #     return hars
+
+	def get_har(self, site):        
+		try:
+			name = site
+			self.proxy.new_har(name)
+			self.driver.set_page_load_timeout(60)
+			self.driver.get(site)
+
+			time.sleep(1)
+			return self.proxy.har
+		except Exception as e:
+			print("Error in getting har: ",str(e))
+			return None
+
+        
+    # calls get_har for each site
+    # takes a list of site urls
+    # returns a dictionary of site url and json har objects
+	def get_hars(self, sites):
+		x = 0
+		hars = []
+		for site in sites:
+			har = self.get_har(site)
+			hars.append(har)
+			x = x + 1
+		return hars
 
 class Har_generator:
 	def __init__(self):
 		
 		self.hars = []
-		# dict={'port':9222}
-		# find_and_kill_processes_by_port(9222)
 		dict={}
 		current_directory = os.getcwd()
 		self.server = Server(current_directory+"/scripts/browsermob-proxy-2.1.4/bin/browsermob-proxy",options=dict)
@@ -54,7 +201,7 @@ class Har_generator:
 
 		#code for caitlyn
 		# dict={'port':9222}
-		# # find_and_kill_processes_by_port(9222)
+		# find_and_kill_processes_by_port(9222)
 		# current_directory = os.getcwd()
 		# self.server = Server(current_directory+"/scripts/browsermob-proxy-2.1.4/bin/browsermob-proxy",options=dict)
 		# self.server.start()
@@ -73,31 +220,12 @@ class Har_generator:
 
 		# self.driver = webdriver.Chrome(options=chrome_options)
 
-
 	def __del__(self):
 		try:
 			self.server.stop()
 			self.driver.quit()
 		except Exception as e:
 			print (str(e))
-			# self.restart_drive()
-
-
-	def restart_drive(self):
-		print("Restarting...")
-		# self.driver.quit()
-		# options = webdriver.ChromeOptions()
-		options = Options()
-		# options.add_argument("--proxy-server={}".format(self.proxy.proxy)) 
-		options.add_argument(f'--proxy-server={self.proxy.proxy}')
-		options.add_argument("--ignore-ssl-errors=yes")
-		options.add_argument("--ignore-certificate-errors")
-		options.add_argument("--headless")
-		options.add_argument("--remote-debugging-port=9222")
-		options.add_argument("--disable-dev-shm-usage")
-		options.add_argument("--no-cache")
-		options.add_argument("--no-sandbox")        
-		self.driver = webdriver.Chrome(options=options)
 
     # loads up a site
     # takes a site url
@@ -107,19 +235,11 @@ class Har_generator:
 			name = site
 			self.proxy.new_har(name)
 			self.driver.set_page_load_timeout(60)
-			# self.driver.get("https://"+site)
 			self.driver.get(site)
 
 			time.sleep(1)
 			return self.proxy.har
 		except Exception as e:
-			# try:
-			# 	self.server.stop()
-			# 	self.driver.quit()
-			# 	time.sleep(1)
-			# except Exception as e:
-			# 	print (str(e))
-			# hm=Har_generator()
 			print("Error in getting har: ",str(e))
 			return None
 
@@ -131,10 +251,8 @@ class Har_generator:
 		x = 0
 		hars = []
 		for site in sites:
-			# print("%d: Working on %s" % (x, site))
 			har = self.get_har(site)
 			hars.append(har)
-			# self.hars.append(har)
 			x = x + 1
 		return hars
 
@@ -155,7 +273,6 @@ class Resource_collector:
 			return self.resources
 		except:
 			return None
-
 
 def dump_json(data, fn):
     with open(fn, 'w') as fp:
@@ -186,6 +303,32 @@ def updateJsonData(filename,tempDict):
 
 		file.close()
 
+# def collectResources(country, resourcesDict):
+#     filename = DATA_PATH + '/alexaTop500SitesCountries.json'
+#     hm = HARGenerator()
+
+#     website_iter = 0
+
+#     try:
+#         websites = read_websites_country(country, filename)
+
+#         for website in websites:
+#             website_iter += 1
+#             if website in resourcesDict:
+#                 continue
+#             Pen.write(
+#                 f"Collecting resources for hostname: {website} Progress: {website_iter + 1}/{len(websites)} "
+#                 f"({(website_iter + 1) * 100 / len(websites):.2f}%)", color='OKCYAN')
+#             # Pen.write("country: " + country + " ,website: " + website + " ,num: " + str(website_iter), color='OKGREEN')
+#             resourcesDict[website] = []
+#             resources = get_resources(website, hm)
+#             if resources == None:
+#                 hm = HARGenerator()
+#                 resources = get_resources(website, hm)
+#             resourcesDict[website] = resources
+#             dump_json(resourcesDict, 'results/resources/resources_' + country + '.json')
+#     finally:
+#         hm.terminate()
 
 def collectResources(country,hostnames,depth):
 	try:
@@ -200,31 +343,38 @@ def collectResources(country,hostnames,depth):
 		hostnametoResourcesMap={}
 		hostnamesToProcess=hostnames
 		print (f"hostnamesToProcess: {len(hostnamesToProcess)} total hostname passed to function: {len(hostnames)} at depth: {depth}")
-		print (str(e))
-
-	temphostnametoResourcesMap={}
-	hm=Har_generator()
-	hostname_iter=0
-	for hostname in hostnamesToProcess:
-		print (f"At Depth {depth} of {country} and {100*hostname_iter/len(hostnamesToProcess)} % done")
-		if hostname_iter%100==0:
-			updateJsonData('results/resources/hostnametoResourcesMap_'+country+'.json',temphostnametoResourcesMap)	
-			temphostnametoResourcesMap={}
-
-		hostname_iter+=1
-		try:
-			resources=get_resources(hostname,hm)
-			temphostnametoResourcesMap[hostname]=resources				
-		except Exception as e:
-			print (f"{hostname} Can't get Har of the hostname. Error: {str(e)}")
-				
+		# print (str(e))
 		
-	updateJsonData('results/resources/hostnametoResourcesMap_'+country+'.json',temphostnametoResourcesMap)
+	hm=Har_generator()
+	# hm=HARGenerator()
+	try:
+		temphostnametoResourcesMap={}
+		hostname_iter=0
+		for hostname in hostnamesToProcess:
+			print (f"At Depth {depth} of {country} and {100*hostname_iter/len(hostnamesToProcess)} % done")
+			if hostname_iter%100==0:
+				updateJsonData('results/resources/hostnametoResourcesMap_'+country+'.json',temphostnametoResourcesMap)	
+				temphostnametoResourcesMap={}
+
+			hostname_iter+=1
+			try:
+				resources=get_resources(hostname,hm)
+				temphostnametoResourcesMap[hostname]=resources				
+			except Exception as e:
+				print (f"{hostname} Can't get Har of the hostname. Error: {str(e)}")
+					
+			
+		updateJsonData('results/resources/hostnametoResourcesMap_'+country+'.json',temphostnametoResourcesMap)
+	finally:
+		# hm.terminate()
+		print ("Done")
+
 	
 	try:
 		resourcesDictwDepth=json.load(open('results/resources/resources_'+str(depth)+"_"+country+'.json'))
 	except:
 		resourcesDictwDepth={}
+
 	hostnametoResourcesMap=json.load(open('results/resources/hostnametoResourcesMap_'+country+'.json'))
 	for hostname in hostnames:
 		resourcesDictwDepth[hostname]=hostnametoResourcesMap[hostname]
